ccab7c9c3b8f3d35ce50e81bd4ab46f80587fa27
jivemachine <the.jeremy.cobb@gmail.com>
2022-04-22 15:03:47 -0500

    add assignment 2

---
+++ assignment2.cpp
@@ -0,0 +1,26 @@
+// Jeremy Cobb
+
+// Assignment 2
+
+// 02-03-2022
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // defining variables
+    string fullName, address, phoneNumber, major;
+    
+    fullName = "Jeremy Cobb";
+    address = "2640 Hunt St, New Braunfels, TX 78130";
+    phoneNumber = "830-822-8488";
+    major = "Computer Science";
+    
+    // function cout's text in single line
+    cout << fullName << '\n' << address << '\n' << phoneNumber << '\n' << major << endl;;
+
+    return 0; // end function
+
+}
